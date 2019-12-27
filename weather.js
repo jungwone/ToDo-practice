0c2e9faae25e3caabc3572123f3f47f1104927ca
spcjw <30407259+spcjw@users.noreply.github.com>
2019-12-27 22:30:11 +0900

    navigator(위치 api 사용)

---
+++ weather.js
@@ -0,0 +1,38 @@
+const API_KEY = "725ef8f6e6ceeef2b1e5e9f01d41b91e";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Cant access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    //get weather.
+  }
+}
+function init() {
+  loadCoords();
+}
+
+init();
